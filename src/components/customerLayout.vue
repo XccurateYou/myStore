c3f77e49bc6e3e20ced9f1bb13efa69e2a78f121
XccurateYou <1943864484@qq.com>
2024-05-30 01:09:52 +0800

    d

--- src/components/customerLayout.vue
+++ src/components/customerLayout.vue
@@ -3,7 +3,7 @@
         <div class="left">
             <div class="room">
                 <div class="roomicon">
-                    <i class='bx bx-building-house' ></i>
+                    <i class='bx bx-home'></i>
                 </div>
                 <div class="roomid">
                     1
@@ -20,11 +20,17 @@
         </div>
         <div class="right">
             <div class="config">
-                <div class="temp">24℃</div>
-                <div class="speed">风速 中风</div>
-                <div class="status">状态 正在送风</div>
+                <div class="temp"><i class='bx bxs-thermometer' ></i>24℃</div>
+                <div class="speed"><i class='bx bx-wind'></i> 中风</div>
+                <div class="status"><i class='bx bx-radio-circle-marked'></i> 正在送风</div>
             </div>
-            
+            <div class="control">
+                <div class="power"><i class='bx bxs-toggle-right' ></i></div>
+                <div class="speedcontrol"><i class='bx bxl-tailwind-css'></i>风速</div>
+            </div>
+            <div class="tempcontrol">
+                <i class='bx bx-minus'></i>温度<i class='bx bx-plus' ></i>
+            </div>
         </div>
     </div>
 </template>
@@ -50,8 +56,8 @@ export default {
     background-position: center;
     border-radius: 10px;
     margin-top: 20px;
-    background:rgba(155, 173, 177, 0.788);
-    border: solid 1px white;
+    background:rgb(11,42,82);
+    border: solid 1px rgba(94, 137, 185, 0.589);
 }
 
     .left {
@@ -60,7 +66,7 @@ export default {
         justify-content: space-between;
         width: 35%;
         height: 550px;
-        border-right: solid 1px white;
+        border-right: solid 1px rgba(94, 137, 185, 0.589);
     }
 
     .room{
@@ -69,7 +75,7 @@ export default {
         justify-content: space-evenly;
         font-size: 256px;
         text-align: center;
-        border-bottom:  solid 1px white;
+        border-bottom:  solid 1px rgba(94, 137, 185, 0.589);
         
     }
 
@@ -79,6 +85,7 @@ export default {
         
     }
     .roomid {
+        color: #e4e4e4;
         text-align: center;
         line-height: 300px;
     }
@@ -94,7 +101,7 @@ export default {
 
     .costicon {
         
-        color: #98fb98;
+        color:rgb(81,131,66);
     }
 
     .costnum{
@@ -102,18 +109,89 @@ export default {
     }
     .right{
         width: 65%;
+        color: #e4e4e4;
+        display: flex;
+        flex-wrap: wrap;
     }
 
     .config {
+        width: 100%;
+        height: 96px;
         display: flex;
-        justify-content: space-evenly;
         font-size: 64px;
-        flex-wrap:wrap
     }
 
-    .temp {
-        text-align: center;
-        font-size: 64px;
+    .temp{
+        border-right:solid 1px rgba(94, 137, 185, 0.589);
+        border-bottom:solid 1px rgba(94, 137, 185, 0.589);
+        flex: 1;
+    }
+
+
+    .speed{
+        border-right:solid 1px rgba(94, 137, 185, 0.589);
+        border-bottom:solid 1px rgba(94, 137, 185, 0.589);
+        flex: 1;
+    }
+
+    .status {
+        border-bottom:solid 1px rgba(94, 137, 185, 0.589);
+        flex: 1.5;
+    }
+
+    .control{
         width: 100%;
+        height: 204px;
+        display: flex;
+    }
+
+    .tempcontrol{
+        width: 100%;
+        height: 250px;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        font-size: 64px;
+    }
+
+    .power{
+        width: 35%;
+        text-align: center;
+        line-height: 200px;
+        font-size: 128px;
+        border-right:solid 1px rgba(94, 137, 185, 0.589);
+        border-bottom:solid 1px rgba(94, 137, 185, 0.589);
+    }
+
+    .speedcontrol{
+        width: 65%;
+        text-align: center;
+        line-height: 200px;
+        font-size: 64px;
+        display: flex;
+        justify-content: center;
+        border-bottom:solid 1px rgba(94, 137, 185, 0.589);
+    }
+
+    .bxs-thermometer{
+        color: rgb(190, 99, 99);
+    }
+
+    .bx-wind{
+        color: rgb(188,203,210);
+    }
+
+    .bx-home{
+        color:rgba(115, 211, 214, 0.548);
+    }
+
+    .bxl-tailwind-css{
+        text-align: center;
+        line-height: 200px;
+        font-size: 128px;
+    }
+
+    .bx-radio-circle-marked{
+        color: #97afbba1;
     }
 </style>
